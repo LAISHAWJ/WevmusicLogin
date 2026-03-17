d9adcd66f84a518e9dbd458561d91d1e8e193025
LAISHAWJ <lw2021-0468@uce.edu.do>
2026-03-17 16:37:26 -0400

    Agregar archivos de proyecto.

---
+++ WevmusicLogin/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WevmusicLogin
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
