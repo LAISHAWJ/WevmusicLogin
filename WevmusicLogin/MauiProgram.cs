93ad0eea3b1511668a580ade40d336c55e0e7f14
LAISHAWJ <lw2021-0468@uce.edu.do>
2026-03-17 16:59:58 -0400

    Configuré el builder de la app MAUI registrando las fuentes personalizadas Lato Bold y Lato Regular para usar en la interfaz. Se agregó animación Lottie musical y se integró SkiaSharp

--- WevmusicLogin/MauiProgram.cs
+++ WevmusicLogin/MauiProgram.cs
@@ -1,25 +1,29 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Maui.Controls.Hosting;
+using SkiaSharp.Views.Maui.Controls.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace WevmusicLogin
+namespace WevmusicLogin;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseSkiaSharp()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("LatoBold.ttf", "Bold");
+                fonts.AddFont("LatoRegular.ttf", "Medium");
+            });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+		builder.Logging.AddDebug();
 #endif
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
